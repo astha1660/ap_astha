ce6a7f58b030f4368351790106f40d8ceadab27e
astha1660 <63048492+astha1660@users.noreply.github.com>
2020-04-11 14:02:27 +0530

    Update scan_cyl.c

--- scan_cyl.c
+++ scan_cyl.c
@@ -29,23 +29,25 @@ void sort(int a[],int size, int flag)
 
 
 int main()
-{
-	int n,max,curr,prev,*a,*q1,*q2;
+{       printf("-------------------------------------------------------");
+	printf("\n\t OPERATING SYSTEM ASSIGNMENT CODE CSE316\n");
+	printf("\tName: Astha    Roll No.:37   section: R18SJ   \n");
+	printf("-------------------------------------------------------\n\n");
+	sleep(2);
+	int n,max,curr,prev,*a,*p1,*p2;
 	int i, j=0,k=0;
                int total =0;
 	int f1,f2,f3,f;
 	char buffer[10],invalid[1];
-	
-	write(1,"Enter size of disk: ",strlen("Enter size of disk: "));
+	printf("\n Kindly Enter the Required Information : \n\n");
+	sleep(2);
+	write(1,"Enter size of the disk:     ",strlen("Enter size of disk:     "));
 	f1=read(0,buffer,sizeof(buffer));
 	max=atoi(buffer);
-	
-	write(1,"Enter number of pending requests: ",strlen("Enter number of pending requests: "));
+        write(1,"Enter the number of pending requests:     ",strlen("Enter number of pending requests:     "));
 	f1=read(0,buffer,sizeof(buffer));
 	n=atoi(buffer);
-	
-	
-	write(1,"Enter the curr request (Should be less than size of size of disk): ",strlen("Enter the curr request (Should be less than size of size of disk): "));
+	write(1,"Enter the current request :     ",strlen("Enter the current request :    "));
 	f1=read(0,buffer,sizeof(buffer));
 	curr=atoi(buffer);
 	if(curr>=max || curr<=0)
@@ -55,7 +57,7 @@ int main()
 		return 0;
 	}
 		
-	write(1,"Enter the previous request (Should be less than size of size of disk): ",strlen("Enter the previous request (Should be less than size of size of disk): "));
+	write(1,"Enter the previous request :     ",strlen("Enter the previous request :     "));
 	f1=read(0,buffer,sizeof(buffer));
 	prev=atoi(buffer);
 	if(prev>=max || prev<=0)
@@ -66,29 +68,31 @@ int main()
 	}
 
 	a=(int*)malloc(n*(sizeof(int)));	
-	q1=(int*)malloc(n*(sizeof(int)));
-	q2=(int*)malloc(n*(sizeof(int)));
+	p1=(int*)malloc(n*(sizeof(int)));
+	p2=(int*)malloc(n*(sizeof(int)));
 		
-	write(1,"Enter the requests (Should be less than size of size of disk): ",strlen("Enter the requests (Should be less than size of size of disk): "));
-	
+	write(1,"Enter the requests in the queue : ",strlen("Enter the requests in the queue : "));
+
 	for(i=0;i<n;i++)
 	{
 		f1=read(0,buffer,sizeof(buffer));
 		a[i]=atoi(buffer);
 		if(a[i]>=max || a[i]<=0)
 		{
+				printf("\n\nCalculating Results...\n");
+	sleep(4);
 			write(1,"Invalid request",strlen("Invalid request"));
 			f2=read(0,invalid,1);
 			return 0;
 		}
 		if(a[i]<curr)
 		{
-			q1[j]=a[i];
+			p1[j]=a[i];
 			j++;	
 		}
 		else
 		{
-			q2[k]=a[i];
+			p2[k]=a[i];
 			k++;	
 		}
 			
@@ -97,32 +101,36 @@ int main()
 	if(curr<prev)
 	{
 		printf("%d",curr);
-		sort(q1,j,1);
+		sort(p1,j,1);
+		
 		printf(" -> 0");
-		sort(q2,k,0);
+		sort(p2,k,0);
 		if(k==0)
 			total= curr;
 		else	
-			total=curr+q2[k-1];
-        	printf("/nTotal distance moved by disk arm is %d",total);						
+			total=curr+p2[k-1];
+				printf("\n\nCalculating Distance...");
+	sleep(4);
+        	printf("\n\nTotal distance moved by disk arm is %d",total);						
 	}
 	else
 	{
 		printf("%d",curr);
-		sort(q2,k,0);
+		sort(p2,k,0);
+		
 		printf("-> %d",max);
-		sort(q1,j,1);
+		sort(p1,j,1);
 		if(j==0)
 			total=max-curr;
 		else	
-			total=max-curr+max-q1[0];
-		printf("\nTotal distance moved by disk arm is %d",total);
+			total=max-curr+max-p1[0];
+				printf("\n\nCalculating Distance...");
+	sleep(4);
+		printf("\n\nTotal distance moved by disk arm is %d",total);
 	}
-	
-	
 	free(a);
-	free(q1);
-	free(q2);
+	free(p1);
+	free(p2);
 	return 0;
 }
 
